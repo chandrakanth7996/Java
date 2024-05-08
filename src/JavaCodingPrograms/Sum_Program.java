2b616db9ad028218e16749fdb28d318a141b2387
Chandrakanth <chandrakanth7996@gmail.com>
2024-05-08 09:04:04 +0530

    Addition program

---
+++ src/JavaCodingPrograms/Sum_Program.java
@@ -0,0 +1,17 @@
+package JavaCodingPrograms;
+
+import java.util.Scanner;
+
+public class Sum_Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the two numbers");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println("Sum of two numbers is: "+ (a+b));
+
+	}
+
+}
