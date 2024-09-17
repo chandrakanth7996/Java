815453823d99f04bcd576b19f076fab02adf58ed
Chandrakanth G <44400111+chandrakanth7996@users.noreply.github.com>
2024-09-17 22:43:52 +0530

    Update Sorting.java

--- Sorting.java
+++ Sorting.java
@@ -1,7 +1,5 @@
 package JavaPrograms;
-
 public class Sorting {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] arr = {32,53,5432,456,54};
@@ -17,7 +15,6 @@ public class Sorting {
 			System.out.print(arr[i]+" "); // 32 53 54 456 5432 
 		}	
 	}
-
 }
 
 
