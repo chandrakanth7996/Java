7e93ff5d70ba0e3ccf1e2bf4beb0448a697bd951
Chandrakanth G <44400111+chandrakanth7996@users.noreply.github.com>
2024-09-17 22:43:30 +0530

    Update Sorting.java

--- Sorting.java
+++ Sorting.java
@@ -19,3 +19,16 @@ public class Sorting {
 	}
 
 }
+
+
+// 2nd way of sorting using Arrays
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+       int[] arr = {1,3,4,2,2,7};
+       Arrays.sort(arr);
+       System.out.println(Arrays.toString(arr));
+    }
+}
+
+// output : [1, 2, 2, 3, 4, 7]
